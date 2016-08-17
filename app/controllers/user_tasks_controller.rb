e9c13bf7175c014c408e3142e3976307ad45458c
Colby Taperts <colbytaperts@gmail.com>
2016-08-17 10:13:05 -0500

    add destroy link with ajax

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,6 +1,6 @@
 class UserTasksController < ApplicationController
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
-  before_action :all_tasks, only: [:index, :create, :update]
+  before_action :all_tasks, only: [:index, :create, :update, :update, :destroy]
 
   # GET /user_tasks
   # GET /user_tasks.json
@@ -61,7 +61,8 @@ class UserTasksController < ApplicationController
   def destroy
     @user_task.destroy
     respond_to do |format|
-      format.html { redirect_to user_tasks_url, notice: 'User task was successfully destroyed.' }
+      format.html { redirect_to user_tasks_url }
+      format.js {}
       format.json { head :no_content }
     end
   end
