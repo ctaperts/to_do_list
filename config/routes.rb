176478493d556fdd82e49fda51ddf49381d7817a
Colby Taperts <colbytaperts@gmail.com>
2016-08-17 08:47:58 -0500

    setup usertask scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :user_tasks
+
+  root 'user_tasks#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
